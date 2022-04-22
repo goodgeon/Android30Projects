1be6e36a8914b3391c1f461147fbb8c065c33c2c
goodgeon <leegg@ais-info.co.jp>
2022-04-22 16:20:57 +0900

    [03 SECRETDIARY] - Project init + Login UI

---
+++ 03SECRETDIARY/app/src/main/java/jp/co/info/ais/a03secretdiary/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.co.info.ais.a03secretdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
