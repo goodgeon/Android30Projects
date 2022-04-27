43c44a3a6afd8c850ec18473e864fd9fe393139f
goodgeon <leegg@ais-info.co.jp>
2022-04-27 17:37:56 +0900

    [04 CALCULATOR] UI

---
+++ 04Calculator/app/src/main/java/example/calculator/MainActivity.kt
@@ -0,0 +1,28 @@
+package example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clearButtonClicked(view:View) {
+
+    }
+
+    fun buttonClicked(view:View) {
+
+    }
+
+    fun historyButtonClicked(view:View) {
+
+    }
+
+    fun resultButtonClicked(view:View) {
+
+    }
+}
