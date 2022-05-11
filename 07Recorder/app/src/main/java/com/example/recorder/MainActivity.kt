a4afb259886c04e7ee57951e962088a01e2a2477
goodgeon <dlrnerjs@gmail.com>
2022-05-11 23:35:29 +0900

    [07Recorder] UI

--- 07Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
+++ 07Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var state: State = State.BEFORE_RECORDING
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        recordButton.updateIconWithState(state)
     }
 }
