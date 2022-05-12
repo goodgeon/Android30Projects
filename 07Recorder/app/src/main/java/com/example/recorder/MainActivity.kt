5a365fec283334bac78351850c4eed708ba5d7b5
goodgeon <leegg@ais-info.co.jp>
2022-05-12 14:52:46 +0900

    [07Recorder] Record audio permission

--- 07Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
+++ 07Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
@@ -1,22 +1,50 @@
 package com.example.recorder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     private var state: State = State.BEFORE_RECORDING
 
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+
     private val recordButton: RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
         initViews()
     }
 
     private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUEST_RECORD_AUDIO_PERMISSION && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
