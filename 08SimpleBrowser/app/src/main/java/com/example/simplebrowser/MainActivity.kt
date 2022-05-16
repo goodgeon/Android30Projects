a0b46f9b89d7a3eae0913dd5e8d4ac4db9962bfa
goodgeon <dlrnerjs@gmail.com>
2022-05-16 22:41:06 +0900

    [08SimpleBrowser] Loading URL, Navigation

--- 08SimpleBrowser/app/src/main/java/com/example/simplebrowser/MainActivity.kt
+++ 08SimpleBrowser/app/src/main/java/com/example/simplebrowser/MainActivity.kt
@@ -1,11 +1,86 @@
 package com.example.simplebrowser
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val DEFAULT_URL = "http://www.google.com"
+    }
+
+    private val goHomeButton: ImageButton by lazy {
+        findViewById(R.id.goHomeButton)
+    }
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
+    private val goBackButton: ImageButton by lazy {
+        findViewById(R.id.goBackButton)
+    }
+
+    private val goForwardButton: ImageButton by lazy {
+        findViewById(R.id.goForwardButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl(DEFAULT_URL)
+        }
+    }
+
+    fun bindViews() {
+        goHomeButton.setOnClickListener {
+            webView.loadUrl(DEFAULT_URL)
+        }
+
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE) {
+                val loadingUrl = v.text.toString()
+                webView.loadUrl(loadingUrl)
+            }
+
+            return@setOnEditorActionListener false
+        }
+
+        goBackButton.setOnClickListener {
+            webView.goBack()
+        }
+
+        goForwardButton.setOnClickListener {
+            webView.goForward()
+        }
     }
 }
