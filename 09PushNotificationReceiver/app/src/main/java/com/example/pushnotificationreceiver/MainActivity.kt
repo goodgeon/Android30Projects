30a70f4c4a1d70939a0a3dbeb28c7422a850e59f
goodgeon <dlrnerjs@gmail.com>
2022-05-18 23:51:19 +0900

    [09PushNotificationReceiver] FCM connected

--- 09PushNotificationReceiver/app/src/main/java/com/example/pushnotificationreceiver/MainActivity.kt
+++ 09PushNotificationReceiver/app/src/main/java/com/example/pushnotificationreceiver/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pushnotificationreceiver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val resultTextView: TextView by lazy {
@@ -15,5 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                firebaseToken.text = task.result
+            }
+        }
     }
 }
