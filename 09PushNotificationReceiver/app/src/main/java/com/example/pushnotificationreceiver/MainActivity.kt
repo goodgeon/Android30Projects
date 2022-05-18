5f1c92d77681c3917031f9bcc54cd41a7b9af070
goodgeon <dlrnerjs@gmail.com>
2022-05-18 21:09:21 +0900

    [09PushNotificationReceiver] Project setup & UI

---
+++ 09PushNotificationReceiver/app/src/main/java/com/example/pushnotificationreceiver/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pushnotificationreceiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
