0975f032698605d11803aa359b459d87beb2acdf
goodgeon <leegg@ais-info.co.jp>
2022-05-26 16:54:30 +0900

    [10DailyQuote] 10DailyQuote

--- 10DailyQuote/app/src/main/java/com/example/dailyquote/MainActivity.kt
+++ 10DailyQuote/app/src/main/java/com/example/dailyquote/MainActivity.kt
@@ -2,14 +2,25 @@ package com.example.dailyquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
 import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
+    }
+
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +32,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        viewPager.adapter = QuotesPagerAdapter(emptyList())
+        viewPager.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue >= 1F -> {
+                    page.alpha = 0F
+                }
+                position.absoluteValue == 0F -> {
+                    page.alpha = 1F
+                }
+                else -> {
+                    page.alpha = 1 - 2 * position.absoluteValue
+                }
+            }
+        }
+
     }
 
     private fun initData() {
@@ -31,5 +55,42 @@ class MainActivity : AppCompatActivity() {
                 minimumFetchIntervalInSeconds = 0
             }
         )
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
+
+            val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
+            val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+            displayQuotesPager(quotes, isNameRevealed)
+        }
+    }
+
+    private fun parseQuotesJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+        var jsonList = emptyList<JSONObject>()
+
+        for (index in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(index)
+
+            jsonObject?.let {
+                jsonList += it
+            }
+        }
+
+        return jsonList.map {
+            Quote(it.getString("quote"), it.getString("name"))
+        }
+    }
+
+    private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
+        viewPager.adapter = QuotesPagerAdapter(quotes, isNameRevealed)
+        viewPager.setCurrentItem(Int.MAX_VALUE / 2 - 3, false)
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Log.d("GG", "${viewPager.currentItem}")
+            }
+        })
     }
 }
