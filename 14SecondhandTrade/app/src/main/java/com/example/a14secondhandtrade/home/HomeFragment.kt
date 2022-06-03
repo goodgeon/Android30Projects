a2aed6b6d834ebffe6e4d2c082d62e1d0207f771
goodgeon <leegg@ais-info.co.jp>
2022-06-03 16:37:43 +0900

    [14SecondhandTrade] HomeFragment list UI

--- 14SecondhandTrade/app/src/main/java/com/example/a14secondhandtrade/home/HomeFragment.kt
+++ 14SecondhandTrade/app/src/main/java/com/example/a14secondhandtrade/home/HomeFragment.kt
@@ -1,7 +1,28 @@
 package com.example.a14secondhandtrade.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a14secondhandtrade.R
+import com.example.a14secondhandtrade.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf(
+            ArticleModel("aaa", "title1", 1000000, "5000원", ""),
+            ArticleModel("bbb", "title2", 2000000, "10000원", "")))
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+    }
 }
